68af72e44c1a8422eca3b4f51437887e42033aad
charlesCaballero <charles.g.caballero@gmail.com>
2023-12-14 17:41:40 +0800

    Ongoing transforming code for responsiveness

--- frontend/src/app/product/FAQSection.tsx
+++ frontend/src/app/product/FAQSection.tsx
@@ -4,6 +4,8 @@ import LeadText from "../components/LeadText";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 import PlusIcon from "@/icons/PlusIcon";
 import MinusIcon from "@/icons/MinusIcon";
+import Image from "next/image";
+import Rectangle from "../components/Rectangle";
 
 const faq = [
   {
@@ -41,23 +43,57 @@ const faq = [
 const FAQSection = () => {
   return (
     <div className="inline-block w-full p-24">
-      <div className="w-1/2 pl-20">
-        <LeadText size="text-[32px]">Frequently asked questions.</LeadText>
-        <div className="py-10">
-          <Accordion>
-            {faq.map((val: any, index) => (
-              <AccordionItem
-                key={index + 1}
-                aria-label={`Accordion ${index + 1}`}
-                title={<LeadText size="text-[18px]">{val.question}</LeadText>}
-                indicator={({ isOpen }) =>
-                  isOpen ? <MinusIcon /> : <PlusIcon />
-                }
-              >
-                {val.answer}
-              </AccordionItem>
-            ))}
-          </Accordion>
+      <div className="flex flex-row">
+        <div className="w-1/2 pl-20 flex-1">
+          <LeadText size="text-[32px]">Frequently asked questions.</LeadText>
+          <div className="py-10">
+            <Accordion>
+              {faq.map((val: any, index) => (
+                <AccordionItem
+                  key={index + 1}
+                  aria-label={`Accordion ${index + 1}`}
+                  title={<LeadText size="text-[18px]">{val.question}</LeadText>}
+                  indicator={({ isOpen }) =>
+                    isOpen ? <MinusIcon /> : <PlusIcon />
+                  }
+                >
+                  {val.answer}
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </div>
+        </div>
+        {/* Collage */}
+        <div className="w-1/2 flex flex-1">
+          <div className="relative w-full flex-1">
+            <Rectangle position="absolute" top="top-0" left="left-[230px]" />
+            <Image
+              src={"/images/photo1.png"}
+              height={360}
+              width={250}
+              alt="photo1"
+              className=" block absolute left-0 right-0 bottom-0 top-0 m-auto z-10"
+            />
+            <Image
+              src={"/images/photo5.png"}
+              height={220}
+              width={150}
+              alt="photo1"
+              className="block absolute right-[190px] -top-12 z-0"
+            />
+            <Image
+              src={"/images/photo7.png"}
+              height={159}
+              width={216}
+              alt="photo1"
+              className="block absolute left-[190px] -bottom-12 z-0"
+            />
+            <Rectangle
+              position="absolute"
+              bottom="bottom-10"
+              right="right-[220px]"
+            />
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/app/product/HeroSection.tsx
+++ frontend/src/app/product/HeroSection.tsx
@@ -9,20 +9,30 @@ import WaveIcon from "@/icons/WaveIcon";
 import CartIcon from "@/icons/CartIcon";
 import StarIcon from "@/icons/StarIcon";
 import LeadText from "../components/LeadText";
+import Photo from "../components/Photo";
 
 const HeroSection = () => {
   return (
     <div className="inline-block w-full">
       {/* Header */}
       <div className="w-full min-h-[36px] bg-[#F9F0E5] flex items-center justify-center">
-        <p className="leading-[35px] font-normal text-[#565656] opacity-80 text-[11px]">
-          CONSCIOUSLY MADE BUTTER SOFT STAPLES FOR EVERY DAY (OR NIGHT) | FREE
-          SHIPPING on orders &gt; $200 | easy 45 day return window.
+        <p className="leading-[35px] font-normal text-[#565656] opacity-80 text-[11px] text-center">
+          <span className="md:inline-block sm:hidden">
+            CONSCIOUSLY MADE BUTTER SOFT STAPLES FOR EVERY DAY (OR NIGHT)
+          </span>
+          <span className="inline-block pl-3 sm:hidden">|</span>
+          <span className="inline-block px-3">
+            FREE SHIPPING on orders &gt; $200
+          </span>
+          <span className="inline-block pr-3 sm:hidden">|</span>
+          <span className="md:inline-block sm:hidden">
+            easy 45 day return window.
+          </span>
         </p>
       </div>
       <div className="px-[6%]">
         {/* Logo */}
-        <div className="py-9">
+        <div className="flex py-9 px-1 lg:justify-start md:justify-center sm:justify-center">
           <Image
             src={"/images/logo.png"}
             alt="Byteex logo"
@@ -31,61 +41,71 @@ const HeroSection = () => {
           />
         </div>
         {/* Hero Content */}
-        <div className="flex flex-row p-2">
-          <div className="w-1/2">
-            <LeadText size="text-[58px]">
+        <div className="grid grid-rows-3 grid-cols-7 gap-1 pt-10">
+          <div className="lg:col-span-3 sm:col-span-full lg:text-left md:text-center sm:text-center w-full">
+            <LeadText size="md:text-[58px] sm:text-[26px]">
               Don&apos;t apologize for being comfortable.
             </LeadText>
-            <div className="p-3">
-              <div className="py-5">
-                <ListwithIcon
-                  icon={<SunMoonIcon width="18" height="18" />}
-                  text="Beautiful, comfortable loungewear for day or night."
-                />
-                <ListwithIcon
-                  icon={<CartIcon width="18" height="18" />}
-                  text="No wasteful extras, like tags or plastic packaging."
-                />
-                <ListwithIcon
-                  icon={<WaveIcon width="15" height="15" />}
-                  text="Our signature fabric is incredibly comfortable — unlike anything you've ever felt."
-                />
-              </div>
-              <Button
-                className="rounded-md bg-[#01005B] px-20 py-8 text-[18px] text-white"
-                endContent={<ArrowIcon />}
-              >
-                Customize Your Outfit
-              </Button>
-            </div>
           </div>
+
           {/* Featured Images */}
-          <div className="w-1/2 flex flex-col items-center justify-center ">
+          <div className="row-span-3 lg:col-span-4 md:col-span-full sm:col-span-full w-full items-center justify-center">
             <div className="flex flex-row items-center justify-center">
-              <Rectangle margin="mr-[-60px]" />
-              <Image
+              <Rectangle
+                margin="mr-[-80px]"
+                height="lg:h-[289px] md:h-[189px]"
+              />
+              <Photo
                 src={"/images/photo1.png"}
                 alt="photo1"
-                width={209}
-                height={317}
-                className="mr-[-30px] relative z-10"
+                width="w-[300px]"
+                height={"h-[400px]"}
+                zIndex="z-10"
+                margin="mr-[-60px]"
               />
-              <Image
-                src={"/images/photo3.jpg"}
+              <Photo
+                src={"/images/photo3.png"}
                 alt="photo1"
-                width={260}
-                height={423}
-                className="border-3 border-white relative z-20"
+                width={"w-[360px]"}
+                height={"h-[523px]"}
+                zIndex="z-20"
+                border="border border-2 border-white"
               />
-              <Image
+              <Photo
                 src={"/images/photo2.png"}
                 alt="photo1"
-                width={209}
-                height={317}
-                className="ml-[-30px] relative z-10"
+                width={"w-[300px]"}
+                height={"h-[400px]"}
+                zIndex="z-10"
+                margin="ml-[-60px]"
               />
-              <Rectangle margin="ml-[-60px]" />
+              <Rectangle
+                margin="ml-[-80px]"
+                height="lg:h-[289px] md:h-[189px]"
+              />
             </div>
+          </div>
+          <div className="row-span-2 md:col-span-3 sm:col-span-full">
+            <div className="py-5">
+              <ListwithIcon
+                icon={<SunMoonIcon width="18" height="18" />}
+                text="Beautiful, comfortable loungewear for day or night."
+              />
+              <ListwithIcon
+                icon={<CartIcon width="18" height="18" />}
+                text="No wasteful extras, like tags or plastic packaging."
+              />
+              <ListwithIcon
+                icon={<WaveIcon width="15" height="15" />}
+                text="Our signature fabric is incredibly comfortable — unlike anything you've ever felt."
+              />
+            </div>
+            <Button
+              className="rounded-md bg-[#01005B] px-20 py-8 text-[18px] text-white"
+              endContent={<ArrowIcon />}
+            >
+              Customize Your Outfit
+            </Button>
           </div>
         </div>
         {/* Customer feedback */}
